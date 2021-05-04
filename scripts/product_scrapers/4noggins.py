f4f14e5b9e124a5b93f3b2a34fac835e63f95b8f
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2021-05-04 16:16:34 -0500

    updated scraper as they tweaked their site.  Hope it works!

--- scripts/product_scrapers/4noggins.py
+++ scripts/product_scrapers/4noggins.py
@@ -5,27 +5,31 @@ def scrape(pbar=None):
     item, price, stock, link = ["", "", "", ""]
     data = []
     name = "4noggins"
-    url = "https://4noggins.com/tobacco/tinned-tobacco.html"
+    url = "https://4noggins.com/collections/tinned-tobacco"
 
     soup = get_html(url)
     next_page = True
     while next_page:
-        for product in soup.find_all("div", class_="product details product-item-details"):
+        for product in soup.find_all("div", class_="product-thumbnail"):
             for element in product.find_all():
                 if element.get("class"):
-                    if " ".join(element.get("class")) == "price":
+                    if " ".join(element.get("class")) == "money":
                         price = element.get_text()
-                    if " ".join(element.get("class")) == "product-item-link":
+                        stock = "In Stock"
+                    if " ".join(element.get("class")) == "product-thumbnail__title":
                         itemraw = element.get_text().strip()
                         item = itemraw.translate({ord(":"): None})
-                        link = element.get("href")
-                    if " ".join(element.get("class")) == "stock unavailable":
-                        stock = "Out of stock"
-                if stock != "Out of stock":
-                    stock = "In Stock"
+                        link = ("https://4noggins.com" + element.get("href"))
+                    if " ".join(element.get("class")) == "product-thumbnail__sold-out sold-out":
+                        stockcheck = element.get_text()
+                        if stockcheck == "Sold Out":
+                            stock = "Out of stock"
+
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
-        if soup.find("a", class_="action next"):
-            soup = get_html(soup.find("a", class_="action next").get("href"))
+        if soup.find("a", class_="button button--primary pagination-button__load-more"):
+            soup = get_html("https://4noggins.com" + soup.find("a",
+                                                               class_="button button--primary pagination-button__load-more").get(
+                "href"))
         else:
             next_page = False
 
