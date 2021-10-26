6b459b2f5c71db8c2da8f15103fe2e195521f275
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2021-10-26 08:21:35 -0500

    Cigars International scraper update

--- scripts/product_scrapers/cigarsintl.py
+++ scripts/product_scrapers/cigarsintl.py
@@ -8,29 +8,29 @@ def scrape(pbar=None):
     url = "https://www.cigarsinternational.com/shop/pipe-tobacco/1800049/"
 
     soup = get_html(url)
+
     next_page = True
     while next_page:
-        for product in soup.find_all("div", class_="offer-prod"):
+        for product in soup.find_all("div", class_="category-list-details"):
             for element in product.find_all():
                 if element.get("class"):
                     if " ".join(element.get("class")) == "price-amount":
                         price = "$" + element.get("data-value")
-                    if " ".join(element.get("class")) == "title-inner":
+                    if " ".join(element.get("class")) == "product-brand-heading":
                         item = element.get_text().strip()
-                    if " ".join(element.get("class")) == "offer-title":
+                    if " ".join(element.get("class")) == "title":
                         link = ("https://www.cigarsinternational.com" + element.get("href"))
-                    if " ".join(element.get("class")) == "offer-stock":
+                    if " ".join(element.get("class")) == "d-inline-block":
                         stock = element.get_text().strip()
                         if stock == "Out Of Stock":
                             stock = "Out of stock"
-
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
-        if soup.find("ul", class_="ui-pagination"):
-            status = soup.find("li", class_="ui-pagination-nav-next")
-            if status.find("span", class_="link-disabled"):
+        if soup.find("ul", class_="pagination"):
+            status = soup.find_all("li", class_="page-item")
+            if status[-1].find("span", class_="link-disabled"):
                 next_page = False
             else:
-                new_url = "https://www.cigarsinternational.com" + status.find("a").get("href")
+                new_url = "https://www.cigarsinternational.com" + status[-1].find("a").get("href")
                 soup = get_html(new_url)
 
     return data
