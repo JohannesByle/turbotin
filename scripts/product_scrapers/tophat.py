f8aae709a97cf550d44d31f219d65f202c2c7b5a
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2021-10-27 14:59:03 -0500

    TopHat scraper update

--- scripts/product_scrapers/tophat.py
+++ scripts/product_scrapers/tophat.py
@@ -9,33 +9,31 @@ def scrape(pbar=None):
     url = "https://tophattobacco.com/pipes-and-pipe-tobacco/pipe-tobacco/"
 
     soup = get_html(url)
-    for bar in soup.find_all("div", class_="mobile-filter-content"):
-        for rebar in bar.find_all("ul", class_="category-nav-list"):
-            for cat in rebar.find_all("li", class_="facet-item"):
-                error = True
-                wait_time = 2.75
-                while error:
-                    try:
-                        new_soup = get_html(cat.find("a").get("href"))
-                        error = False
-                    except:
-                        time.sleep(wait_time)
-                        print("An Error Occurred: sleeping " + str(wait_time) + "s")
-                        wait_time = wait_time + 1
-                        pass
-                for product in new_soup.find_all("div", class_="product-item-details"):
-                    for element in product.find_all():
-                        if element.get("class"):
-                            if " ".join(element.get("class")) == "price-value-wrapper":
-                                price = element.get_text().strip()
-                            stock = "In Stock"
-                            if " ".join(element.get("class")) == "product-item-title":
-                                item = element.find("a").get_text().strip()
-                                link = element.find("a").get("href")
-                    sub_soup = get_html(link)
-                    if sub_soup.find(class_="stock-message"):
-                        stock = sub_soup.find(class_="stock-message").get_text().strip()
 
-                    item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
+    for bar in soup.find_all("li", class_="subcategory-item"):
+        error = True
+        wait_time = 2.75
+        while error:
+            try:
+                new_soup = get_html(bar.find("a", class_="subcategory-link").get("href"))
 
+                error = False
+            except:
+                time.sleep(wait_time)
+                print("An Error Occurred: sleeping " + str(wait_time) + "s")
+                wait_time = wait_time + 1
+                pass
+        for product in new_soup.find_all("div", class_="card-body"):
+            if product.find("h4", class_="card-title"):
+                item = product.find("h4", class_="card-title").get_text().strip()
+            if product.find("a", class_="image-link desktop"):
+                link = product.find("a", class_="image-link desktop").get("href")
+            if product.find("span", class_="price price--withoutTax"):
+                price = product.find("span", class_="price price--withoutTax").get_text()
+            if product.find("a", class_="button button--small card-figcaption-button"):
+                stock = "In Stock"
+            else:
+                stock = "Out of stock"
+
+            item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
     return data
