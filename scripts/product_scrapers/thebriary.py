13450c3c6b34733b24759a30ebdefce571cb909e
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2022-12-04 15:28:29 -0600

    TheBriary site changed.  Updated scraper works with new link

--- scripts/product_scrapers/thebriary.py
+++ scripts/product_scrapers/thebriary.py
@@ -5,32 +5,27 @@ def scrape(pbar=None):
     item, price, stock, link = ["", "", "", ""]
     data = []
     name = "thebriary"
-    url = "http://www.thebriary.com/tobacco.html"
+    url = "https://thebriary.com/tobacco/tin-tobacco/"
 
     soup = get_html(url)
     next_page = True
     while next_page:
-        for product in soup.find_all("div", class_="product-item"):
-            for element in product.find_all():
-                if element.get("class"):
-                    if " ".join(element.get("class")) == "price":
-                        priceraw = element.get_text()
-                        head, sep, price = priceraw.partition(":")
-                        price = price.strip()
-                    if " ".join(element.get("class")) == "status":
-                        stock = element.get_text().strip()
-                    if " ".join(element.get("class")) == "name":
-                        item = element.get_text().strip()
-                        link = ("http://www.thebriary.com/" + element.find("a").get("href"))
-                if stock == "OUT OF STOCK" or stock == "Out of Stock":
-                    stock = "Out of stock"
-
+        for product in soup.find_all("li", class_="product"):
+            stock = "Out of stock"
+            if product.find("h3", class_="card-title"):
+                item = product.find("h3", class_="card-title").get_text().strip()
+                link = product.find("a").get("href")
+            if product.find("span", class_="price--withoutTax"):
+                price = product.find("span", class_="price--withoutTax").get_text().strip()
+            if product.find("a", class_="button"):
+                if product.find("a", class_="button").get_text().strip() == "Add to Cart":
+                    stock = "In Stock"
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
-        for page in soup.find_all(class_="paging"):
-            if page.find("a", string="Next Page"):
-                next_url = ("http://www.thebriary.com/" + page.find("a", string="Next Page").get("href"))
-                soup = get_html(next_url)
-            else:
-                next_page = False
+        if soup.find("li", class_="pagination-item--next"):
+            status = soup.find("li", class_="pagination-item--next")
+            new_url = status.find("a", class_="pagination-link").get("href")
+            soup = get_html(new_url)
+        else:
+            next_page = False
 
     return data
