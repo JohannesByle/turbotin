20d28da5f113d70bcf514826106697d169761ff6
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2022-12-07 23:40:37 -0600

    Tophat In Stock data was not coming through correctly.  Fix it by finding new datapoint to target

--- scripts/product_scrapers/tophat.py
+++ scripts/product_scrapers/tophat.py
@@ -26,14 +26,14 @@ def scrape(pbar=None):
         for product in new_soup.find_all("div", class_="card-body"):
             if product.find("h4", class_="card-title"):
                 item = product.find("h4", class_="card-title").get_text().strip()
-            if product.find("a", class_="image-link desktop"):
-                link = product.find("a", class_="image-link desktop").get("href")
+                link = product.find("a").get("href")
             if product.find("span", class_="price price--withoutTax"):
                 price = product.find("span", class_="price price--withoutTax").get_text()
-            if product.find("a", class_="button button--small card-figcaption-button"):
-                stock = "In Stock"
-            else:
-                stock = "Out of stock"
+            if product.find("a", class_="button"):
+                if product.find("a", class_="button").get("href") == "#":
+                    stock = "Out of stock"
+                else:
+                    stock = "In Stock"
 
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
     return data
