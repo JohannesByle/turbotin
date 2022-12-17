bf0a81e586b81ddcf7df0ca2a851ced8f2d0d9e4
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2022-12-17 09:06:59 -0600

    Fixed LilBrown scraper - their site was updated yesterday to new format

--- scripts/product_scrapers/lilbrown.py
+++ scripts/product_scrapers/lilbrown.py
@@ -6,19 +6,31 @@ def scrape(pbar=None):
     item, price, stock, link = ["", "", "", ""]
     data = []
     name = "lilbrown"
-    url = "https://www.lilbrown.com/c-202-pipe-tobacco.aspx"
+    url = "https://www.lilbrown.com/product-category/pipe-tobacco/"
 
     soup = get_html(url)
-    for category in soup.find("div", class_="tabbitTabWrap").find_all("td", class_="subEntityCell"):
-        new_soup = get_html("https://www.lilbrown.com/" + category.find("a").get("href"))
-        for product in new_soup.find_all("div", class_="productGridCellContents"):
-            price = product.find("div", class_="price-wrap").get_text()
-            price = re.findall(r"\$\d{1,3}\.\d{2}", price)[-1]
-            item = product.find(class_="productGridNameWrap").find("a").get_text()
-            link = "https://www.lilbrown.com/" + product.find("a").get("href")
-            if "Buy Now" in product.find(class_="stock-hint").get_text():
-                stock = "In stock"
-            else:
+    for cat in soup.find_all("li", class_="product-category product first"):
+        link = cat.find("a").get("href")
+        error = True
+        wait_time = 2.75
+        while error:
+            try:
+                new_soup = get_html(link)
+                error = False
+            except:
+                time.sleep(wait_time)
+                print("An Error Occurred: sleeping " + str(wait_time) + "s")
+                wait_time = wait_time + 1
+        for element in new_soup.find_all('div', class_="thunk-product"):
+            if element.find("h2", class_="woocommerce-loop-product__title"):
+                item = element.find("h2", class_="woocommerce-loop-product__title").get_text().strip()
+            if element.find("a"):
+                link = element.find("a").get("href")
+            if element.find("bdi"):
+                price = element.find("bdi").get_text().strip()
+            if element.find("a", class_="button").get_text().strip() == "Add to cart":
+                stock = "In Stock"
+            if element.find("a", class_="button").get_text().strip() == "Read more":
                 stock = "Out of stock"
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
 
