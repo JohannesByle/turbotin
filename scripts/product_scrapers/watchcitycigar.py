8ae919459886cc79597e31f2b417fc2199d2de52
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2022-12-19 15:52:39 -0600

    WatchCity wasn't showing accurate Out of stock data, so changed scraper to look for "add to cart" for accuracy

--- scripts/product_scrapers/watchcitycigar.py
+++ scripts/product_scrapers/watchcitycigar.py
@@ -10,26 +10,21 @@ def scrape(pbar=None):
     soup = get_html(url)
     next_page = True
     while next_page:
-        for product in soup.find_all("li", class_="product"):
-            for element in product.find_all():
-                if element.get("class"):
-                    if " ".join(element.get("class")) == "price-section":
-                        price = element.get_text().strip()
-                    if " ".join(element.get("class")) == "card-title":
-                        item = element.get_text().strip()
-                        link = element.find("a").get("href")
-                    if " ".join(element.get("class")) == "sale-text":
-                        if element.get_text().strip() == "Out of stock":
-                            stock = element.get_text().strip()
-                    if stock != "Out of stock":
-                        stock = "In Stock"
-
-            item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
+        for product in soup.find_all("ul", class_="productGrid"):
+            for element in product.find_all("li", class_="product"):
+                stock = "Out of stock"
+                if element.find("span", class_="price price--withoutTax price--main"):
+                    price = element.find("span", class_="price price--withoutTax price--main").get_text().strip()
+                if element.find("h4", class_="card-title"):
+                    item = element.find("h4", class_="card-title").get_text().strip()
+                    link = element.find("a").get("href")
+                if element.find("a", class_="button").get_text().strip() == "Add to Cart":
+                    stock = "In Stock"
+                item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
         if soup.find("li", class_="pagination-item--next"):
             status = soup.find("li", class_="pagination-item--next")
             new_url = status.find("a", class_="pagination-link").get("href")
             soup = get_html(new_url)
         else:
             next_page = False
-
     return data
