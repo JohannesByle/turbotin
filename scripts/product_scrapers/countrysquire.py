ea218acd6341ab907f496dd796f60c82d027728d
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-01-04 18:13:42 -0600

    Updated Countrysquire scrape

--- scripts/product_scrapers/countrysquire.py
+++ scripts/product_scrapers/countrysquire.py
@@ -12,27 +12,25 @@ def scrape(pbar=None):
     soup = get_html(url)
     next_page = True
     while next_page:
-        for products in soup.find_all("script", type="text/template"):
-            if "product-col" in products.string:
-                products_soup = BeautifulSoup(json.loads(products.string), "lxml")
-                for product in products_soup.find_all("li"):
-                    item = product.find("h3").text
-                    if product.find("ins"):
-                        price = product.find("ins").text
-                    else:
-                        if product.find("bdi"):
-                            price = product.find("bdi").text
-                    if product.find("div", class_="out-of-stock"):
-                        stock = "Out of stock"
-                    else:
-                        stock = "In Stock"
-                    link = product.find("a").get("href")
-                    item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
+        for product in soup.find_all("li", class_="ast-col-sm-12"):
+            item = product.find("h2").text
+            if product.find("ins"):
+                price = product.find("ins").text
+            else:
+                if product.find("bdi"):
+                    price = product.find("bdi").text
+            if product.find("a", class_="button").text == "Read more":
+                stock = "Out of stock"
+            if product.find("a", class_="button").text == "Add to cart":
+                stock = "In Stock"
+            link = product.find("a").get("href")
+            item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
 
         if soup.find("a", class_="next page-numbers"):
             new_url = soup.find("a", class_="next page-numbers").get("href")
             soup = get_html(new_url)
         else:
             next_page = False
+
 
     return data
