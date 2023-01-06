129e8169b5310b16856e2ead92109e9dcde850c5
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-01-05 20:16:40 -0600

    updating init_py with a user agent that can be used to bypass in Cloudflare

--- scripts/product_scrapers/__init__.py
+++ scripts/product_scrapers/__init__.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def get_html(url):
     req = request.Request(url, headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46 | Turbotin ',
         'Referer': 'https://www.turbotin.com'})
     response = request.urlopen(req)
     return BeautifulSoup(response.read(), features="lxml")
