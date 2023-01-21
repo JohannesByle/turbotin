fa46f43eeb3267e2aa7f8a7b2de9250d906ca706
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-01-20 23:56:26 -0600

    Fixed WindyCityCigars scraper - their site must have changed and we were not getting item details

--- scripts/product_scrapers/windycitycigars.py
+++ scripts/product_scrapers/windycitycigars.py
@@ -10,18 +10,17 @@ def scrape(pbar=None):
     soup = get_html(url)
     next_page = True
     while next_page:
-        for product in soup.find_all("div", class_="box-text"):
-            for element in product.find_all():
-                if element.get("class"):
-                    if " ".join(element.get("class")) == "price":
-                        if element.find("ins"):
-                            price = element.find("ins").get_text().strip()
-                        else:
-                            price = element.get_text().strip()
-                    if " ".join(element.get("class")) == "name product-title":
-                        item = element.get_text().strip()
-                        link = element.find("a").get("href")
-                        stock = "In Stock"
+        for element in soup.find_all("div", class_="box-text"):
+            if element.find("p", class_="product-title"):
+                item = element.find("p", class_="product-title").get_text().strip()
+            if element.find("a", class_="woocommerce-LoopProduct-link"):
+                link = element.find("a", class_="woocommerce-LoopProduct-link").get("href")
+            if element.find("div", class_="price-wrapper"):
+                price = element.find("div", class_="price-wrapper").get_text().strip()
+            if element.find("ins"):
+                price = element.find("ins").get_text().strip()
+            stock = "In Stock"
+
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
         if soup.find("a", class_="next page-number"):
             soup = get_html(soup.find("a", class_="next page-number").get("href"))
