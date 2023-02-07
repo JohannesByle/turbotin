5a5ac025a0eecbf4503fd2ac83b4825a61eef490
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-02-06 23:54:13 -0600

    Updated Pipenook scraper as they are selling tobacco again

--- scripts/product_scrapers/pipenook.py
+++ scripts/product_scrapers/pipenook.py
@@ -6,34 +6,23 @@ def scrape(pbar=None):
     item, price, stock, link = ["", "", "", ""]
     data = []
     name = "pipenook"
-    url = "https://www.thepipenook.com/store/c14/pipe-tobacco"
+    url = "https://thepipenook.com/tobacco/"
 
     soup = get_html(url)
-    for cat in soup.find_all("a", class_="wsite-com-category-subcategory-link"):
-        error = True
-        wait_time = 2.75
-        while error:
-            try:
-                new_soup = get_html("https://www.thepipenook.com" + cat.get("href"))
-                wait_time = 2.75
-                error = False
-            except:
-                time.sleep(wait_time)
-                print("An Error Occurred: sleeping " + str(wait_time) + "s")
-                wait_time = wait_time + 1
-            pass
-            for product in new_soup.find_all('div', class_="wsite-com-category-product-wrap"):
-                if product.find("div",
-                                class_="wsite-com-category-product-name wsite-com-link-text"):
-                    item = product.find("div",
-                                        class_="wsite-com-category-product-name wsite-com-link-text").get_text().strip()
-                link = "https://www.thepipenook.com" + product.find("a").get("href")
-                if product.find("div", class_="wsite-com-price"):
-                    price = product.find("div", class_="wsite-com-price").get_text().strip()
-                stock = "In Stock"
-                if product.find_all("p", class_="category__out-of-stock-badge"):
-                    stock = "Out of stock"
-
-                item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
-
+    next_page = True
+    while next_page:
+        for element in soup.find_all("div", class_="card-body"):
+            if element.find("h3", class_="card-title"):
+                item = element.find("h3", class_="card-title").get_text().strip()
+                link = element.find("a").get("href")
+            if element.find("span", class_="price price--withoutTax"):
+                price = element.find("span", class_="price price--withoutTax").get_text().strip()
+            stock = "In Stock"
+            item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
+        if soup.find("li", class_="pagination-item--next"):
+            links = soup.find("li", class_="pagination-item--next")
+            link = links.find("a")
+            soup = get_html(link.get("href"))
+        else:
+            next_page = False
     return data
