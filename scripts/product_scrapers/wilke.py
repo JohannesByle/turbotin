4f65f969c925d241f44130b764ec87107bde68e5
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-03-05 22:15:44 -0600

    Updated Wilke scraper for updated class details

--- scripts/product_scrapers/wilke.py
+++ scripts/product_scrapers/wilke.py
@@ -9,6 +9,7 @@ def scrape(pbar=None):
 
     soup = get_html(url)
     for cats in soup.find_all("div", class_="KcpHeO"):
+        # print(cats)
         error = True
         wait_time = 2.75
         for cat in cats.find_all("h2", class_="font_2"):
@@ -23,12 +24,12 @@ def scrape(pbar=None):
                         print("An Error Occurred: sleeping " + str(wait_time) + "s")
                         wait_time = wait_time + 1
                         pass
-                for product in new_soup.find_all("ul", class_="S4WbK_ c2Zj9x"):
+                for product in new_soup.find_all("ul", class_="S4WbK_ uQ5Uah c2Zj9x"):
                     for element in product.find_all("div", class_="ETPbIy WJH1qD"):
                         stock = ""
                         if element.find("div", class_="CZ0KIs"):
-                            if element.find("h3", class_="syQOIUy"):
-                                item = element.find("h3", class_="syQOIUy").get_text().strip()
+                            if element.find("h3"):
+                                item = element.find("h3").get_text().strip()
                             if element.find("a", class_="JPDEZd"):
                                 link = element.find("a", class_="JPDEZd").get("href")
                         if element.find("span", class_="cfpn1d"):
@@ -37,6 +38,7 @@ def scrape(pbar=None):
                             stock = element.find("span", class_="zOeYx3").get_text().strip()
                         if stock != "Out of stock":
                             stock = "In Stock"
+
                         item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
 
     return data
