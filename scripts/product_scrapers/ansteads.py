4f34cd2076ed52519abdb341a024758921b69956
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-06-13 14:58:45 -0500

    Updated Ansteads scraper with a resoup to get out of stock info.

--- scripts/product_scrapers/ansteads.py
+++ scripts/product_scrapers/ansteads.py
@@ -12,12 +12,13 @@ def scrape(pbar=None):
     while next_page:
         for element in soup.find_all("div", class_="product-block"):
             if element.find("h4"):
-                stock = ""
+                stock = "In Stock"
                 item = element.find("h4").get_text().strip()
                 link = element.find("a", class_="title").get("href")
             if element.find("div", class_="product-block-price"):
                 price = element.find("div", class_="product-block-price").get_text().strip()
-            if element.find("div", class_="product-block-label label-outofstock"):
+            resoup = get_html(link)
+            if resoup.find("dd", class_="col-xs-7 out-of-stock"):
                 stock = "Out of stock"
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
         if soup.find("li", class_="next"):
