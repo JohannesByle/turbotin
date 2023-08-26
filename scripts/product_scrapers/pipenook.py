51d74010dffd09d7a47a5e52c6eefa587bf86c9b
warrenbyle <62268776+warrenbyle@users.noreply.github.com>
2023-08-26 13:42:20 -0500

    updating Pipenook scraper for better in stock details

--- scripts/product_scrapers/pipenook.py
+++ scripts/product_scrapers/pipenook.py
@@ -11,13 +11,15 @@ def scrape(pbar=None):
     soup = get_html(url)
     next_page = True
     while next_page:
-        for element in soup.find_all("div", class_="card-body"):
+        for element in soup.find_all("article", class_="card"):
             if element.find("h3", class_="card-title"):
                 item = element.find("h3", class_="card-title").get_text().strip()
                 link = element.find("a").get("href")
             if element.find("span", class_="price price--withoutTax"):
                 price = element.find("span", class_="price price--withoutTax").get_text().strip()
             stock = "In Stock"
+            if element.find("a", class_="button").get_text() == "Out of stock":
+                stock = "Out of stock"
             item, price, stock, link = add_item(data, name, item, price, stock, link, pbar)
         if soup.find("li", class_="pagination-item--next"):
             links = soup.find("li", class_="pagination-item--next")
