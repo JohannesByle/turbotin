83711ae5fa78db7794a6cb0ab080f8611e92aa0d
Johannes <johannesbyle1@gmail..com>
2023-10-23 20:50:35 -0500

    try to optimize font loading

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,7 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import { cyan, grey, yellow } from "@mui/material/colors";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
