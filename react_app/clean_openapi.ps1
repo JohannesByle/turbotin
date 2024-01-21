177cf7841a060db0deb7a3f0d6ba9ae40587baf3
Rachel Barron <johannesbyle1@gmail.com>
2024-01-21 17:18:33 -0600

    remove unused 2: electric boogalo'

--- react_app/clean_openapi.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem ".\src\service" -Recurse -File
-foreach ($file in $files) {  
-    Set-Content $file.FullName ((Get-Content $file.FullName) -replace "\/\* tslint:disable \*\/",       "")
-}
-
-npx prettier --write ./src/service
